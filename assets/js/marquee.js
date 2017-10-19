3eb178f3d505cada278f8359deef8b08b4a08356
jensschnitzler <jens.schnitzler@gmail.com>
2017-10-19 14:49:16 +0200

    Add "marquee.js"
    marquee.js makes the vertical title banner on the right work

---
+++ assets/js/marquee.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+
+  function marquee() {
+
+    var marquee = $( ".marquee" ); // get my marquee cotainer
+    var marqTop = $( marquee ).offset().top; // get my marquee cotainer's inner width
+    console.log( "marqTop: " + marqTop );
+    
+    var pLast = marquee.children('p').last(); // get my last p
+    pLast.addClass( "marker" );
+    var pText = pLast.text(); // get my last p's text
+    console.log( pText );
+
+    var pLastTop = Math.ceil( marqTop - pLast.offset().top ); // calculate distance between my last p's right side and the parent container
+    console.log( "pLastTop: " + pLastTop );
+    
+    if( pLastTop < 0 ) { // if p exceeds marquee
+      $( ".marker" ).removeClass( "marker" );
+      //$( marquee ).append("<p>" + pText + "</p>"); // add new p;
+      $( "<p>" + pText + "</p>" ).addClass( "marker" ).appendTo( marquee ); // add new p;
+    }    
+  }
+  marquee();
+  
+  $(window).scroll(function() {
+    marquee();
+    var myElements = $( ".marquee p" );
+    myScroll = $(window).scrollTop(); // vertical scroll position
+    myElements.css({"transform" : "translateX(" + -myScroll + "px)" });
+  });
+});
+
