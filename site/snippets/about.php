772b375f049da6dafd4c3794df28cb617802e415
Tim Rausch <timrausch@gmx.de>
2017-10-25 23:54:33 +0200

    Corrected about.php

--- site/snippets/about.php
+++ site/snippets/about.php
@@ -1,15 +1,19 @@
 <div class="about-container overlay">
 
   <div id="about-content">
-  	<h1><?= $page->title()->html() ?></h1>
-	<p><?= $page->text()->kirbytext() ?></p>
+      <?php
+      $about = $pages->find('about');
+      ?>
+
+  	<h1><?= $about->title()->html() ?></h1>
+	<p><?= $about->text()->kirbytext() ?></p>
   </div>
 
   <div id="network-list">
-  	<h2><?= $page->networkListTitle()->html() ?></h2>
+  	<h2><?= $about->networkListTitle()->html() ?></h2>
 
 	<ul class="network-list">
-	  <?php foreach($page->networks()->toStructure() as $network): ?>
+	  <?php foreach($about->networks()->toStructure() as $network): ?>
 	  <li>
 	    <a href="<?php echo $network->url() ?>">
 	      <?php echo $network->name()->html() ?>
@@ -22,11 +26,15 @@
   <div id="about-info">
   	<ul class="margin-info">
 		<li>
-			<div id="about-image" class="interviewee-image">
-				<img src="<?= $page->aboutImage()->url() ?>" alt="About Image" />
-			</div>
+            <?php foreach($about->images() as $image): ?>
+                <div id="about-image" class="interviewee-image">
+                    <a href="<?php echo $image->url() ?>">
+                        <img src="<?php echo $image->url() ?>" alt="" style="width: 500px">
+                    </a>
+                </div>
+            <?php endforeach ?>
 		</li>
-		<li><?= $page->imageCaption()->html() ?></li>
+		<li><?= $about->imageCaption()->html() ?></li>
 	</ul>
   </div>
 
