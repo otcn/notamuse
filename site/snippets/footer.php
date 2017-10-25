b910550e8ce78c359b45df6bf36bbd89511ecdb2
Tim Rausch <timrausch@gmx.de>
2017-10-25 19:21:53 +0200

    Changed snippet/template structure

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,13 +1,14 @@
-  <footer class="footer cf" role="contentinfo">
+<footer class="footer cf" role="contentinfo">
     <div class="wrap wide">
-
-      <p class="footer-copyright"><?php
-        // Parse Kirbytext to support dynamic year,
-        // but remove all HTML like paragraph tags:
-        echo html::decode($site->copyright()->kirbytext())
-      ?></p>
+        <p class="footer-copyright">
+            <?php
+            // Parse Kirbytext to support dynamic year,
+            // but remove all HTML like paragraph tags:
+            echo html::decode($site->copyright()->kirbytext())
+            ?>
+        </p>
     </div>
-  </footer>
+</footer>
 
 </body>
 </html>
--- site/templates/home.php
+++ site/templates/home.php
@@ -2,8 +2,14 @@
 
 snippet('header');
 
+snippet('aside');
+
+snippet('article');
+
+/*
 foreach($pages->visible() as $section) {
   snippet($section->uid(), array('data' => $section));
 }
+*/
 
 ?>
