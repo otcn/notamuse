f5f60f7e43fff3578cfdcd84953207c8338405a4
jensschnitzler <jens.schnitzler@gmail.com>
2017-10-29 12:48:56 +0100

    commented home.php

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,3 +1,4 @@
+<!-- the following php places all snippets concerning the main stage -->
 <?php
 
 snippet('header');
@@ -5,6 +6,15 @@ snippet('header');
 snippet('aside');
 
 snippet('article');
+
+?>
+
+<!-- the "separator" divides the main stage and the overlays. on click overlays close -->
+<div id="separator"><a class="key-icon active"></a></div>
+
+
+<!-- the following php places all snippets concerning the overlays -->
+<?php
 
 snippet('about');
 
@@ -18,7 +28,7 @@ foreach($pages->visible() as $section) {
 
 ?>
 
-<!-- marquee is the vertical notamuse banner on the right window edge. should this be placed directly in here or be placed as a snippet? -->
+<!-- the following php places the marquee snippet. the marquee is the vertical notamuse banner on the right window edge. should this be placed directly in here or be placed as a snippet? -->
 <?php
 snippet('marquee');
 ?>
