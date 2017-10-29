0d5855beeb994f5664dc386ddbbda18d7b57ae9f
jensschnitzler <jens.schnitzler@gmail.com>
2017-10-29 11:43:20 +0100

    edited about.php and about-contents

--- site/accounts/admin.php
+++ site/accounts/admin.php
@@ -7,8 +7,8 @@ email: timrausch@gmx.de
 language: en
 role: admin
 history:
+  - about
   - intro
   - intro/intro
-  - about
   - interviews/grafikladen
   - themen/arbeitsprozess/habt-ihr-eine-taegliche-routine
--- site/snippets/about.php
+++ site/snippets/about.php
@@ -1,41 +1,37 @@
 <div class="about-container overlay">
 
   <div id="about-content">
-      <?php
+    <?php
       $about = $pages->find('about');
-      ?>
-
+    ?>
   	<h1><?= $about->title()->html() ?></h1>
-	<p><?= $about->text()->kirbytext() ?></p>
+    <p><?= $about->text()->kirbytext() ?></p>
   </div>
 
   <div id="network-list">
   	<h2><?= $about->networkListTitle()->html() ?></h2>
-
-	<ul class="network-list">
-	  <?php foreach($about->networks()->toStructure() as $network): ?>
-	  <li>
-	    <a href="<?php echo $network->url() ?>">
-	      <?php echo $network->name()->html() ?>
-	    </a>
-	  </li>
-	  <?php endforeach ?>
-	</ul>
+  	<ul class="network-list">
+  	  <?php foreach($about->networks()->toStructure() as $network): ?>
+  	  <li>
+  	    <a href="<?php echo $network->url() ?>">
+  	      <?php echo $network->name()->html() ?>
+  	    </a>
+  	  </li>
+  	  <?php endforeach ?>
+  	</ul>
   </div>
 
-  <div id="about-info">
-  	<ul class="margin-info">
-		<li>
-            <?php foreach($about->images() as $image): ?>
-                <div id="about-image" class="interviewee-image">
-                    <a href="<?php echo $image->url() ?>">
-                        <img src="<?php echo $image->url() ?>" alt="" style="width: 500px">
-                    </a>
-                </div>
-            <?php endforeach ?>
-		</li>
-		<li><?= $about->imageCaption()->html() ?></li>
-	</ul>
+  <div id="about-info" class="margin-info">
+    <figure>
+      <?php foreach($about->images() as $image): ?>
+        <div id="about-image" class="interviewee-image">
+            <a href="<?php echo $image->url() ?>">
+                <img src="<?php echo $image->url() ?>" alt="" style="width: 500px">
+            </a>
+        </div>
+      <?php endforeach ?>
+      <figcaption><?= $about->imageCaption()->html() ?></figcaption>
+    </figure>
   </div>
 
 </div>
