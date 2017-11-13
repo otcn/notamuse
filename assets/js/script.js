7a48c293af6f68ff7fced3225082cdb394aed8bc
zweipi <jana@zwei-pi.de>
2017-11-13 20:57:25 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,18 +6,20 @@ $(document).ready(function() {
 
   // History
   function projects(uid) {
-    $.ajax({
-      url: '/' + uid, // url: 'http://localhost/~jensschnitzler/' + uid,
-      type: 'POST',
-      success: function(response) {
-          $('.interview-container').html(response);
-          $('.interview-container').removeClass('hidden');
-          $('#separator').removeClass('hidden');
-      },
-      error: function() {
-          console.log('ajax error');
-      }
-    });
+    if (uid !== 'about'){
+      $.ajax({
+        url: '/' + uid, // url: 'http://localhost/~jensschnitzler/' + uid,
+        type: 'POST',
+        success: function(response) {
+            $('.interview-container').html(response);
+            $('.interview-container').removeClass('hidden');
+            $('#separator').removeClass('hidden');
+        },
+        error: function() {
+            console.log('ajax error');
+        }
+      });
+    }
   }
 
   History.Adapter.bind(window, 'statechange', function() {
@@ -38,6 +40,10 @@ $(document).ready(function() {
     if (uid.length <= 0){  // if it’s not a subpage, show intro and seperator */
       $( '.intro-container' ).removeClass('hidden'); // should be ".removeClass('hidden')" later
       $( '#separator' ).removeClass('hidden'); // should be ".removeClass('hidden')" later
+    }
+    else if (uid == 'about'){
+      $('.about-container').removeClass('hidden');
+      $('#separator').removeClass('hidden');
     }
     else{
       $( '#separator' ).removeClass('hidden'); // if it’s subpage, only show the seperator
@@ -68,15 +74,16 @@ $(document).ready(function() {
   $('.nav-about a').click(function(){
       $('.about-container').removeClass('hidden');
       $('#separator').removeClass('hidden');
+      push('about');
   });
 
   /* on mouseenter the interview should load into the container */
-  $('.nav-interview a, a.interviewee-title').mouseenter( function(e) {
+  $('a.interviewee-title').mouseenter(function(e) {
     var uid = $(this).attr('imgsrc'); // get image source
     $('.preview img').attr('src', uid);
     $('.preview figure').show();
   });
-  $('.nav-interview a, a.interviewee-title').mouseleave( function(e) {
+  $('a.interviewee-title').mouseleave(function(e) {
     $('.preview figure').hide();
     $('.preview img').attr('src', ''); // clear image source
   });
