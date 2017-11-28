33bb1da0c569253de3af1240f878d3200aefe428
jensschnitzler <jens.schnitzler@gmail.com>
2017-11-28 23:15:31 +0100

    alphabetical order

--- site/snippets/navigation.php
+++ site/snippets/navigation.php
@@ -6,46 +6,59 @@
 
       <li>
         <a id="questions-button" class="nav-switch" href="#">Fragen</a><a class="key-icon"></a>
-        <div id="sl-1" class="sub">
-          <ul>
-            <?php
-              // build an array of all unique question objects
-              $fragenDone = array();
-              $lettersDone = array();
-              $newLetter = false;
-              foreach($pages->find('interviews')->children()->visible() as $interview) {
-              	foreach($interview->interview()->toStructure()->sortBy('frage', 'asc') as $frage) {
-                  // make sure we show each question only once
-                  if (! in_array($frage->frage(), $fragenDone)) {
-  						      array_push($fragenDone, $frage->frage());
-                    // determine link target for question
-  						      if ($frage->spezialfrage()->bool() === true) {
-  						        $openwraptag = '<a href="'.$interview->url().'#'.$frage->fid().'">'; // link directly to interview page/section
-  						        $closewraptag = '</a>';
-  						      } else {
-  						        $openwraptag = '<a href="#'.$frage->fid().'">'; // link to question in topics
-  						        $closewraptag = '</a>';
-  						      }
-        						// new first letter? show it!
-        						$letter = substr($frage->frage(),0,1);
-        						if (! in_array($letter, $lettersDone)) {
-        							array_push($lettersDone, $letter);
-        						  ?>
-        						  <li class="register-mark"><?= $letter?></li>
-        						  <?php
-        						  $newletter = true;
-        						}
-        						?>
-        						<li class="nav-question">
-        							<?php echo $openwraptag.$frage->frage()->html().$closewraptag;?>
-        						</li>
-        						<?php
-        					}
-        				}
-        			}
-            ?>
-          </ul>
-        </div>
+          <?php
+            function cmp($a, $b) {
+              return strcmp($a->frage(), $b->frage());
+            }
+           ?>
+
+          <div id="sl-1" class="sub">
+            <ul>
+              <?php
+                // build an array of all unique question objects
+                $fragenDone = array();
+                $lettersDone = array();
+                $newLetter = false;
+                foreach($pages->find('interviews')->children()->visible() as $interview) {
+                  foreach($interview->interview()->toStructure() as $frage) {
+                    // make sure we show each question only once
+                    if (! in_array($frage, $fragenDone)) {
+                      array_push($fragenDone, $frage);
+                    }
+                  }
+                }
+
+                // sort questions alphabetically
+                usort($fragenDone, "cmp");
+
+                // output
+                foreach ($fragenDone as $frage) {
+                  // determine link target for question
+                  if ($frage->spezialfrage()->bool() === true) {
+                    $openwraptag = '<a href="'.$interview->url().'#'.$frage->fid().'">'; // link directly to interview page/section
+                    $closewraptag = '</a>';
+                  } else {
+                    $openwraptag = '<a href="#'.$frage->fid().'">'; // link to question in topics
+                    $closewraptag = '</a>';
+                  }
+                  // new first letter? show it!
+                  $letter = substr($frage->frage(),0,1);
+                  if (! in_array($letter, $lettersDone)) {
+                    array_push($lettersDone, $letter);
+                    ?>
+                    <li class="register-mark"><?= $letter?></li>
+                    <?php
+                    $newletter = true;
+                  }
+                  ?>
+                  <li class="nav-question">
+                    <?php echo $openwraptag.$frage->frage()->html().$closewraptag;?>
+                  </li>
+                  <?php
+                }
+              ?>
+            </ul>
+          </div>
       </li>
 
       <li>
@@ -82,6 +95,7 @@
           </ul>
         </div>
       </li>
+
       <li class="nav-spacer"></li>
       <li class="nav-about"><a id="about-button" class="nav-button about-anchor">About</a></li>
       <li><a id="language-button" class="nav-button language-anchor">EN</a></li>
