ece9a84b402810a3ab73ec05ddf732e16635ae3c
jensschnitzler <jens.schnitzler@gmail.com>
2017-12-06 20:55:19 +0100

    improved header.php and added error page

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -27,6 +27,8 @@
   <link rel="icon" type="image/png" sizes="16x16" href="/path/to/favicon-16x16.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/path/to/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="96x96" href="/path/to/favicon-96x96.png">
+  <!-- Apple Touch Icon 180×180px -->
+  <link rel="apple-touch-icon" href="icon.png">
   <!-- remove mobile top bar -->
   <meta name="apple-mobile-web-app-capable" content="yes" />
 
